cf43abc2d0413ba484744d3d325898c81ca0834b
melked <ce.melikedemirtas@gmail.com>
2023-05-21 22:16:18 +0300

    update

--- db_conn.php
+++ db_conn.php
@@ -1,8 +1,8 @@
 <?php
-$sname = "localhost";
-$uname = "root";
-$password = "";
-$db_name = "test_db";
+$sname = "sql101.epizy.com";
+$uname = "epiz_34255809";
+$password = "v112cjSu15";
+$db_name = "epiz_34255809_test_db";
 
 $conn = mysqli_connect($sname, $uname, $password, $db_name);
 if (!$conn) {
--- home.php
+++ home.php
@@ -1,24 +1,10 @@
 <?php
 session_start();
-if(isset($_SESSION['id']) && isset($_SESSION['user_name'])){
-    ?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>HOME</title>
-        <link rel ="stylesheet" type="text/css" href="style.css">
-
-    </head>
-<body>
-    <h1>Hello,<?php echo $_SESSION['user_name']; ?></h1>
-    <a href ="logout.php">Logout</a>
-</body>
-</html>
-<?php
-}
-else{
-    header("location:index.php");
+if(isset($_SESSION['user_name'])){
+    $username = $_SESSION['user_name'];
+    echo "Merhaba, " . $username . "! kullanıcı adım";
+    header("Location: hakkimda.html");
     exit();
 }
 ?>
+
